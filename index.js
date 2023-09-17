9324282efb103183616f36a87b048b46ad30bc91
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-09-17 01:24:55 +0100

    Update index.js
    Updated Firebase version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://champions-25567-default-rtdb.europe-west1.firebasedatabase.app/"
