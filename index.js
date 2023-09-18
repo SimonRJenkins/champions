83105eceb22b2fb0e4a1be762931df943beeb364
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-09-18 01:19:37 +0100

    To/From input fields
    Added input fields for users to put their name and the name of the person they want to endorse
    Added styling for the To and From values for when an endorsement is published

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ const database = getDatabase(app)
 const endorsementsInDB = ref(database, "endorsements")
 
 const inputFieldEl = document.getElementById("input-field")
+const inputFromEl = document.getElementById("input-from")
+const inputToEl = document.getElementById("input-to")
 const publishButtonEl = document.getElementById("publish-btn")
 const endorsementEl = document.getElementById("endorsements")
 
 publishButtonEl.addEventListener("click", function () {
     let inputValue = inputFieldEl.value
+
 
     push(endorsementsInDB, inputValue)
 
@@ -51,21 +54,38 @@ function clearEndorsementsEl() {
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
+    inputFromEl.value = "From"
+    inputToEl.value = "To"
 }
 
 function appendItemToEndorsementEl(item) {
     let itemID = item[0]
     let itemValue = item[1]
 
+    let inputTo = inputToEl.value
+    let inputFrom = inputFromEl.value
+
     let newBox = document.createElement("div")
     let newEl = document.createElement("p")
+    let newFrom = document.createElement("p")
+    let newTo = document.createElement("p")
 
     newEl.textContent = itemValue
+    newFrom.textContent = "From: " + inputFrom
+    newTo.textContent = "To: " + inputTo
+
 
     newBox.setAttribute("class", "endorsements")
     newEl.setAttribute("id", "endorsement")
 
+    newTo.style.fontSize = "13px";
+    newTo.style.fontWeight = "bold";
+    newFrom.style.fontSize = "13px";
+    newFrom.style.fontWeight = "bold";
+
+    newBox.appendChild(newTo)
     newBox.appendChild(newEl)
+    newBox.appendChild(newFrom)
 
     newEl.addEventListener("click", function () {
         let exactLocationOfItemInDB = ref(database, `endorsements/${itemID}`)
@@ -74,3 +94,11 @@ function appendItemToEndorsementEl(item) {
 
     endorsementEl.appendChild(newBox)
 }
+
+
+// Add styling for to and from on the endorsement
+// Add a like button and styling
+// Prevent the publish button being pressed when input field, to and from is empty
+// sort text alignment for no current endorsements and new endorsements
+// sort out space between endorsements, should be a black gap between each one
+
